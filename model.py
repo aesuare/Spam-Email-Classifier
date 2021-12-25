e6af869d925e64de23ef133012b646898ee1e062
Alfredo Suarez <asuarez4704@gmail.com>
2021-12-24 20:30:06 -0500

    revamp

---
+++ model.py
@@ -0,0 +1,10 @@
+# Import packages
+import os
+import io
+import numpy
+import pandas
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# Read files and import data
+print("Hello, world!")
